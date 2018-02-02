9684a67eaba10ffaa526f4f0cb2757853cbe9959
Poipt <34775545+Poipt@users.noreply.github.com>
2018-02-02 12:45:13 -0800

    Update __init__.py

--- mathextras/__init__.py
+++ mathextras/__init__.py
@@ -104,6 +104,25 @@ def prime_out(num):
     print("--------------------------------------------------") 
 
     return
+def mode(nums):
+    output = {}
+    lsnum = None
+    lock = 0
+    amount = 0
+    for x in range(len(nums)):
+        if lock == 0:
+            lock = 1
+            lsnum = nums[x]
+        if nums[x] != lsnum:
+            output[lsnum] = amount
+            lsnum = nums[x]
+            amount = 0
+            continue
+        else:
+            amount += 1
+            continue
+    return output
+
 
 def mean(nums):
   output = 0
