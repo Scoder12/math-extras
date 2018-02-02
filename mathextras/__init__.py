28aa12c0a14b2da1d1839c3e5f3f3ec2be363991
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2018-02-01 16:57:25 -0800

    Put working code in

--- mathextras/__init__.py
+++ mathextras/__init__.py
@@ -1,1 +1,20 @@
+"""__init__.py
+Calls other files in the package.
+"""
 
+def prime(num):
+    import prime
+    return prime.prime(num)
+
+def fac(num):
+    import faclist
+    out = faclist.getfac(num)
+    return out
+
+def lcm(num1, num2, limit=10):
+    import lcm
+    return lcm.findlcm(num1, num2, limit)
+
+def prime_out(num):
+    import prime_out
+    return prime_out.prime_out(num)
