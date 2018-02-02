5ec0a6f092e9b18447da4cbf90c20fe31359191a
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2018-02-01 17:09:36 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='funniest',
+setup(name='mathextras',
       version='1.0',
       description='Adds prime, lcm, and factors functionality.  ',
       url='https://github.com/Scoder12/mathextras',
