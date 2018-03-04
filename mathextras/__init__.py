434543b0801d6c1cf70b4eb75c49035b6f04c6dd
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2018-03-04 15:23:48 -0800

    Change pi (see descriptiuon) and started docstrings

--- mathextras/__init__.py
+++ mathextras/__init__.py
@@ -1,23 +1,36 @@
 """__init__.py
 Calls other files in the package.
 """
+# π constant (500 digits)
+PI = 3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460955058223172535940812848111745028410270193852110555964462294895493038196442881097566593344612847564823378678316527120190914564856692346034861045432664821339360726024914127372458700660631558817488152092096282925409171536436789259036001133053054882046652138414695194151160943305727036575959195309218611738193261179310511854807446237996274956735188575272489122793818301194912
+# π symbol (just because)
+PI_SYMBOL = "π"
 
-def pi():
-	return 22 / 7
-
-def bubbleSort(alist):
-    for passnum in range(len(alist)-1,0,-1):
-        for i in range(passnum):
-            if alist[i]>alist[i+1]:
-                temp = alist[i]
-                alist[i] = alist[i+1]
-                alist[i+1] = temp
-    return alist
+def bubbleSort(inlist):
+	"""BubbleSort: Returns the array specified sorted  
+	Args:
+		ainlist (array): inlist to be sorted
+	Returns:
+		array: inlist sorted from greatest to least
+	Code credit: interactivepython.org
+	"""
+	for passnum in range(len(inlist)-1,0,-1):
+		for i in range(passnum):
+			if inlist[i]>inlist[i+1]:
+				temp = inlist[i]
+				inlist[i] = inlist[i+1]
+				inlist[i+1] = temp
+	return inlist
 
 def fac(num):
-    # Gets list of factors of variable num
+    """fac: Returns an array of factors for the given number
+    Args: 
+    	num (int): number to list factors for
+    Returns:
+    	array: factors of num
+    """
     output = []
-    # Creates list for output
+    # Creates inlist for output
     for x in range(1, num + 1):
     # Loop with variable x
         if num % x == 0 and not num % x in output:
@@ -26,6 +39,13 @@ def fac(num):
     return output
 
 def lcm(num1, num2, limit=10):
+  """lcm: finds the least common multiple of two numbers
+  Args:
+	num1 (int): First number to calculate lcm of
+	num2 (int): Second number to calculate lcm of
+  Returns:
+	int: the least common multiple (lcm) of num1 and num2
+  """
   num1ls = [num1]
   num2ls = [num2]
   mult = list(range(2, limit + 1))
@@ -42,37 +62,37 @@ def lcm(num1, num2, limit=10):
 
 
 def prime (num):
-    """prime(num)
-    Returns true or false depending on whether or not a number is prime.
-    num is and int to process.  
+    """prime: calclates whether or not a number is prime
+    Args:
+    	num (int): Number to be evaluated as prime
+    Returns:
+    	bool: True if the number is prime, False otherwise
+    Note: prime() is mostly for internal use, for a more user User-friendly version,
+    see prime_out()
     """
-    import time
-    strnum = str(num)
     count = num - 1
     def remain (div):
-        quotient = num / div
-        strdiv = str(div)
-        foo = num % div
-        if foo == 0:
+        ans = num % div
+        if ans == 0:
+        	# Divides, evenly, so not prime
             return True
         else:
+        	# Try again
             return False
     while count > 1:
         if remain (count):
             return False
-            count = 1
         else:
             count = count - 1
+    # We're out of the loop, so it must be prime
     return True
-    return
 
 def prime_out(num):
-    """prime_out(num)
-    User-friendly version of prime().
-    Prints output and has timing functionality.
-    Usage:
-    prime_out(num)
-    num is an int that to be evaluated as prime.  
+    """prime_out: A user-friendly tool to evaluate whether or not a number is prime. 
+    Args:
+    	num (int): Number to be evaluated as prime
+    Returns:
+    	Prints whether or not the number is prime and how long it took to evaluate.   
     """
     import time
     t1_start = time.perf_counter()
@@ -81,7 +101,6 @@ def prime_out(num):
     count = num - 1
     print("Calculating whether " + strnum + " is prime...")
     def remain (div):
-        quotient = num / div
         strdiv = str(div)
         foo = num % div
         if foo == 0:
@@ -101,7 +120,6 @@ def prime_out(num):
             print("or %.1f [min]" % ((t1_stop-t1_start)/60))
             print("--------------------------------------------------") 
             return
-            count = 1
         else:
             count = count - 1
     t1_stop = time.perf_counter()
@@ -117,43 +135,64 @@ def prime_out(num):
     return
 
 def mean(nums):
-    output = 0
-    for x in range(len(nums)):
-        output += nums[x]
-    return output / len(nums)
+	"""mean: finds the mean of a list of numbers
+	Args:
+		nums (array): At least two numbers to find the mean of.
+	Returns:
+		Float: the exact mean of the numbers in the array.  
+	"""
+	output = 0
+	for x in range(len(nums)):
+		output += nums[x]
+	return output / len(nums)
     
 
 def mode(inlist):
-    bubbleSort(inlist)
-    streak = 1
-    array = 1
-    longest = 1
-    modeo = "nan"
-    while array < len(inlist) - 1:
-        if inlist[array] == inlist[array + 1]:
-            streak = streak + 1
-        else:
-            if streak > longest:
-                longest = streak
-                modeo = inlist[array]
-                streak = 1
-        array = array + 1
-    if modeo == "nan":
-        modeo = 0
-    return modeo
+	"""mode: finds the mode of an array of numbers
+	
+	Important note: This code does not support multiple modes!
+	Even though technically there can be more than one mode of
+	a data set, this code will return None if there is no clear
+	mode, and only the first mode if there is more than one mode.
+	We are sorry for the inconvenience, and hope to add this
+	feature soon!
+	
+	Args:
+		inlist (array): An array of numbers to find the mode of
+	Returns:
+		Int: the mode of the numbers given
+		None if there is no clear mode
+	"""
+	bubbleSort(inlist)
+	streak = 1
+	array = 1
+	longest = 1
+	modeo = "nan"
+	while array < len(inlist) - 1:
+		if inlist[array] == inlist[array + 1]:
+			streak = streak + 1
+		else:
+			if streak > longest:
+				longest = streak
+				modeo = inlist[array]
+				streak = 1
+				array = array + 1
+	if modeo == "nan":
+		modeo = None
+	return modeo
 
-def median(listin):
-	bubbleSort(listin)
-	listo = 0
-	length = len(listin)
+def median(inlistin):
+	bubbleSort(inlistin)
+	inlisto = 0
+	length = len(inlistin)
 	for x in range(int(length / 2) - 1):
-		del listin[0]
-		del listin[-1]
-	if len(listin) == 2:
-		listo = (int(listin[0] + listin[1]) / 2)
+		del inlistin[0]
+		del inlistin[-1]
+	if len(inlistin) == 2:
+		inlisto = (int(inlistin[0] + inlistin[1]) / 2)
 	else:
-		listo = listin[1]
-	return listo
+		inlisto = inlistin[1]
+	return inlisto
 
 def gcf(nums1, nums2):
 	nums1 = fac(nums1)
